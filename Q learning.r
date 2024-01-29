ab8382d284e9bcb2c1b3ca44e2cb4a4dc8326184
2Duffman <moritzbeyer@t-online.de>
2024-01-29 15:03:16 +0100

    implement training for different stocks, output still noise

--- Q learning.r
+++ Q learning.r
@@ -5,12 +5,14 @@
 # JNJ - Johnson & Johnson
 # agent should be able to buy and sell these stocks each day. Use q learning to find the best strategy.
 # Load the stock data from data.csv
-stock_data <- read.csv("historical data/MSFT.csv")
+stock_files <- list.files("data", full.names = TRUE)
+stock_data <- lapply(stock_files, read.csv)
 # Set the agent parameters
-max_money <- 100000 # Maximum amount of money available
-max_stocks <- 300 # Maximum number of stocks available
-num_stocks <- 1 # Number of different stocks
+max_money <- 10000 # Maximum amount of money available
+max_stocks <- 10 # Maximum number of stocks available
+num_stocks <- 3 # Number of different stocks
 max_transaction <- 10 # maximum number of stocks to buy/sell each day
+max_day <- min(sapply(stock_data, nrow)) # Number of days to train on
 
 # Set the learning parameters
 num_episodes <- 1000  # Number of episodes
@@ -19,19 +21,20 @@ gamma <- 0.9  # Discount factor
 epsilon <- 0.1  # Exploration rate
 
 # Define the maximum and minimum states
-max_state <- c(max_money, max_stocks)
-min_state <- c(0, 0)
+max_state <- c(max_money, max_stocks, max_stocks, max_stocks)
+min_state <- c(0, 0, 0, 0)
 
 # Initialize the Q-table with zeros
-num_states <- max_money * max_stocks * num_stocks
 num_actions <- 3 * num_stocks * max_transaction
-Q <- array(0, dim = c(max_state[1] - min_state[1] + 1, max_state[2] - min_state[2] + 1, num_actions))
+Q <- array(0, dim = c(max_state[1] - min_state[1] + 1, max_state[2] - min_state[2] + 1, max_state[3] - min_state[3] + 1, max_state[4] - min_state[4] + 1, num_actions))
 
 # Define the reward function
-reward <- function(money, stocks, prices,
+reward <- function(illegal_move, money, stocks, prices,
                                      money_yesterday, stocks_yesterday, prices_yesterday, first_day) {
     if (first_day) {
         return(0)
+    } else if (illegal_move) {
+        return(-1000)
     } else {
         return(sum(money + stocks * prices) - sum(money_yesterday + stocks_yesterday * prices_yesterday))
     }
@@ -40,34 +43,20 @@ reward <- function(money, stocks, prices,
 # Define the Q-learning algorithm
 for (episode in 1:num_episodes) {
     # Initialize the state
-    state <- c(10000, 0)
+    state <- c(1000, 0, 0, 0)
     
-    # Iterate over the stock data
-    consecutive_dates <- 0
-    for (i in 1:nrow(stock_data)) {
+    for (i in 1:max_day) {
         # Check if the current date is the same as the previous date
-        if (i > 1 && stock_data$Date[i] == stock_data$Date[i-1]) {
-            consecutive_dates <- consecutive_dates + 1
-        } else {
-            consecutive_dates <- 1
-        }
         
-        # Choose an action if consecutive dates reach 5,
-        # this ensures the agent only operates on days where all stocks are available
-        if (consecutive_dates == num_stocks) {
             if (runif(1) < epsilon) {
                 action <- sample(0:(num_actions-1), 1)
                 #print(paste("random action: ", action))
             } else {
-                action <- which.max(Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, ])
+                action <- which.max(Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, state[3] - min_state[3] + 1, state[4] - min_state[4] + 1, ])
                 if (action >= num_actions) {
                     action <- num_actions - 1
                 }
                 #print(paste("max action: ", action))
-            }
-            
-            # Reset consecutive_dates counter
-            consecutive_dates <- 0
         }
     
         
@@ -83,14 +72,17 @@ for (episode in 1:num_episodes) {
         action_amount <- action %% max_transaction
         #print(paste("action_amount: ", action_amount))
         #Price of the picked stock
-        Price <- stock_data$Close[i]
+        Price <- round(stock_data[[action_stock-1]]$Close[i])
+        illegal_move <- FALSE
         if (action_category == 0) {
             # buy stock
             if (action_amount * Price > state[1]) {
+                illegal_move <- TRUE
                 action_amount <- floor(state[1] / Price)
             }
             if (action_amount + state[action_stock] > max_stocks) {
                 action_amount <- max_stocks - state[action_stock]
+                illegal_move <- TRUE
             }
             new_state[action_stock] <- state[action_stock] + action_amount
             new_state[1] <- state[1] - Price * action_amount
@@ -98,6 +90,11 @@ for (episode in 1:num_episodes) {
             # sell stock
             if (action_amount  > state[action_stock]) {
                 action_amount <- state[action_stock]
+                illegal_move <- TRUE
+            }
+            if (action_amount * Price + state[1] > max_money) {
+                action_amount <- floor((max_money - state[1]) / Price)
+                illegal_move <- TRUE
             }
             new_state[action_stock] <- state[action_stock] - action_amount
             new_state[1] <- state[1] + Price * action_amount
@@ -106,18 +103,14 @@ for (episode in 1:num_episodes) {
             new_state <- state
         }
         # Calculate the reward
-        r <- reward(new_state[1], new_state[2], Price,
-                    state[1], state[2], ifelse(i > 1, stock_data$Close[i-1], 0), i == 1)
-        # Check if indices are within the bounds
-        if (new_state[1] - min_state[1] + 1 > dim(Q)[1] || new_state[2] - min_state[2] + 1 > dim(Q)[2]) {
-            print(paste("new_state: ", new_state[1], new_state[2]))
-            print(paste("min_state: ", min_state[1], min_state[2]))
-        } else {
-            # Update the Q-table
-            Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, action + 1] <- Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, action + 1] + alpha * (r + gamma * max(Q[new_state[1] - min_state[1] + 1, new_state[2] - min_state[2] + 1, ]) - Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, action + 1])
-        }  
+        r <- reward(illegal_move, new_state[1], new_state[action_stock], Price,
+                    state[1], state[action_stock], ifelse(i > 1, stock_data[[action_stock-1]]$Close[i-1], 0), i == 1)
+        
+        # Update the Q-table
+        Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, state[3] - min_state[3] + 1, state[4] - min_state[4] + 1, action + 1] <- Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, state[3] - min_state[3] + 1, state[4] - min_state[4] + 1, action + 1] + alpha * (r + gamma * max(Q[new_state[1] - min_state[1] + 1, new_state[2] - min_state[2] + 1, new_state[3] - min_state[3] + 1, new_state[4] - min_state[4] + 1, ]) - Q[state[1] - min_state[1] + 1, state[2] - min_state[2] + 1, state[3] - min_state[3] + 1, state[4] - min_state[4] + 1, action + 1])
         # Update the state
         state <- new_state
 }
-    print(paste("Episode", episode, "completed with net worth: ", state[1] + state[2] * stock_data$Close[nrow(stock_data)]))
+    networth <- sum(state[1], state[2] * stock_data[[1]]$Close[max_day], state[3] * stock_data[[2]]$Close[max_day], state[4] * stock_data[[3]]$Close[max_day])
+    print(paste("Episode", episode, "completed with net worth: ", networth))
 }
